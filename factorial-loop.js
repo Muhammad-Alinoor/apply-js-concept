97d84d8639a32692a162ec56c6754dbdc22fd52f
Muhammad Alinoor <alinoor145387@outlook.com>
2021-08-04 18:18:24 +0600

    factorial-using-function-added

--- factorial-loop.js
+++ factorial-loop.js
@@ -1,12 +1,28 @@
 // count from the given number to 1
+console.log("factorial using loop from the given number to 1");
 let n = 7;
 for (let i = n - 1; i >= 1; i--) {
     n = n * i;
-    console.log(n);
+
 }
+console.log(n);
 // count from 1 to given number 
+console.log("factorial using loop from 1 to given number");
 n = 1;
 for (let i = 1; i <= 7; i++) {
     n = n * i;
-    console.log(n);
+
 }
+console.log(n);
+// factorial using function
+console.log("Factorial using functon");
+function getFactorial(number) {
+    let factorial = 1;
+    for (let i = 1; i <= number; i++) {
+        factorial = factorial * i;
+    }
+    return factorial;
+}
+let num = 10;
+const firstFactorial = getFactorial(num);
+console.log("Factorial of ", num, "is ", firstFactorial);
