7f393808e33fc376f916fc5f128085830629d38c
Muhammad Alinoor <alinoor145387@outlook.com>
2021-08-04 17:36:36 +0600

    factorial-using-loop

---
+++ factorial-loop.js
@@ -0,0 +1,12 @@
+// count from the given number to 1
+let n = 7;
+for (let i = n - 1; i >= 1; i--) {
+    n = n * i;
+    console.log(n);
+}
+// count from 1 to given number 
+n = 1;
+for (let i = 1; i <= 7; i++) {
+    n = n * i;
+    console.log(n);
+}
